63a86e51479c7b6ccf3643b9ea145313341584c6
Ran <xyfwdy@gmail.com>
2023-06-20 01:23:00 +0800

    first commit

---
+++ src/main/java/cc/ranmc/sign/SignApi.java
@@ -0,0 +1,38 @@
+package cc.ranmc.sign;
+
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SignApi extends JavaPlugin implements Listener {
+    @Getter
+    private static SignApi instance;
+    @Getter
+    private static SignFactory factory;
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("§e-----------------------");
+        Bukkit.getConsoleSender().sendMessage("§bSignMenuFactory §dBy Ranica");
+        Bukkit.getConsoleSender().sendMessage("§bVersion: "+getDescription().getVersion());
+        Bukkit.getConsoleSender().sendMessage("§bBukkit: "+getServer().getVersion());
+        Bukkit.getConsoleSender().sendMessage("§chttps://www.ranmc.cc/");
+        Bukkit.getConsoleSender().sendMessage("§e-----------------------");
+        instance = this;
+        factory = new SignFactory(this);
+        super.onEnable();
+    }
+
+    public static SignFactory.Menu newMenu(String text) {
+        return SignApi.getInstance().getFactory().newMenu(Arrays.asList(text, "", "", ""));
+    }
+
+    public SignFactory.Menu newMenu(List<String> text) {
+        return SignApi.getInstance().getFactory().newMenu(text);
+    }
+
+
+}
---
+++ src/main/java/cc/ranmc/sign/SignFactory.java
@@ -0,0 +1,86 @@
+package cc.ranmc.sign;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.BlockPosition;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiPredicate;
+
+public final class SignFactory {
+    private final SignApi plugin;
+    private final Map<Player, Menu> inputs = new HashMap<>();
+
+    public SignFactory(SignApi plugin) {
+        this.plugin = plugin;
+        this.listen();
+    }
+
+    public Menu newMenu(List<String> text) {
+        return new SignFactory.Menu(text);
+    }
+
+
+    private void listen() {
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(plugin, PacketType.Play.Client.UPDATE_SIGN) {
+            @Override
+            public void onPacketReceiving(PacketEvent event) {
+                Player player = event.getPlayer();
+
+                Menu menu = inputs.remove(player);
+
+                if (menu == null) {
+                    return;
+                }
+                event.setCancelled(true);
+
+                boolean success = menu.response.test(player, event.getPacket().getStringArrays().read(0));
+
+                if (!success) Bukkit.getScheduler().runTaskLater(plugin, () -> menu.open(player), 2L);
+                Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                    if (player.isOnline()) {
+                        Location location = menu.position.toLocation(player.getWorld());
+                        player.sendBlockChange(location, location.getBlock().getBlockData());
+                    }
+                }, 2L);
+            }
+        });
+    }
+
+    public final class Menu {
+
+        private final List<String> text;
+        private BiPredicate<Player, String[]> response;
+        private BlockPosition position;
+        Menu(List<String> text) {
+            this.text = text;
+        }
+        public Menu response(BiPredicate<Player, String[]> response) {
+            this.response = response;
+            return this;
+        }
+        public void open(Player player) {
+            Objects.requireNonNull(player, "player");
+            if (!player.isOnline()) return;
+            Location location = player.getLocation().clone();
+            location.setY(location.getBlockY() - 4);
+            player.sendBlockChange(location, Material.CHERRY_SIGN.createBlockData());
+            player.sendSignChange(location, text.toArray(new String[4]));
+            PacketContainer openSign = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.OPEN_SIGN_EDITOR);
+            position = new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+            openSign.getBlockPositionModifier().write(0, position);
+            ProtocolLibrary.getProtocolManager().sendServerPacket(player, openSign);
+            inputs.put(player, this);
+        }
+    }
+}
